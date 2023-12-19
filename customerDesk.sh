ef06964e349aaca3438983e64314bc920d3e319d
Kousik Roy <83376797+KousikRoy-EC@users.noreply.github.com>
2023-12-19 11:46:55 +0530

    Update customerDesk.sh

--- customerDesk.sh
+++ customerDesk.sh
@@ -5,11 +5,11 @@ bill_file="billReport.txt"
 
 totaBill=0
 
-addItem(){
-echo "$1 $2" >> "$temp_file"
+addItem() {
+    echo "$1 $2" >>"$temp_file"
 }
 
-salesReport(){
+salesReport() {
 
     local db="item.txt"
     local temp_file="temp_data.txt"
@@ -17,104 +17,99 @@ salesReport(){
 
     while IFS=" " read -r hsn new_qty; do
         if grep -q "$hsn" "$sales_file"; then
-            awk -v hsn="$hsn" -v new_qty="$new_qty" '$1 == hsn { $2 += new_qty }1' "$sales_file" > "$temp_file"
+            awk -v hsn="$hsn" -v new_qty="$new_qty" '$1 == hsn { $2 += new_qty }1' "$sales_file" >"$temp_file"
             mv "$temp_file" "$sales_file"
         else
-            echo "$hsn $new_qty" >> "$sales_file"
+            echo "$hsn $new_qty" >>"$sales_file"
         fi
-    done < "$db"
+    done <"$db"
 }
 
-updateWarehouse(){
-local temp_file="temp_data.txt"
-local db="item.txt"
+updateWarehouse() {
+    local temp_file="temp_data.txt"
+    local db="item.txt"
 
-while IFS=" " read -r hsn prod_desc new_qty mrp; do
-    if grep -q "$hsn " "$db"; then
-        result=$(grep "$hsn" "$db")
-        up_quant=$(echo "$result" | awk -F' ' '{print $2}')  
-        new_qty=$((new_qty - up_quant))
-        totalBill=$((totalBill + (mrp * up_quant)))
-    fi
-    echo "$hsn $prod_desc $new_qty $mrp" >> "$temp_file"
-done < "$file_name"
+    while IFS=" " read -r hsn prod_desc new_qty mrp; do
+        if grep -q "$hsn " "$db"; then
+            result=$(grep "$hsn" "$db")
+            up_quant=$(echo "$result" | awk -F' ' '{print $2}')
+            new_qty=$((new_qty - up_quant))
+            totalBill=$((totalBill + (mrp * up_quant)))
+        fi
+        echo "$hsn $prod_desc $new_qty $mrp" >>"$temp_file"
+    done <"$file_name"
 
-rm "data.txt"
-rm "item.txt"
-mv "temp_data.txt" "data.txt"
+    rm "data.txt"
+    rm "item.txt"
+    mv "temp_data.txt" "data.txt"
 }
 
-billReport(){
-echo "$1 $2" >> "$bill_file"
-totalBill=0
+billReport() {
+    echo "$1 $2" >>"$bill_file"
+    totalBill=0
 }
-
-
-while true
-do
-
-echo "Enter Your Name!"
-read custName
 
 while true; do
 
-echo "Enter HSN code"
-read HSN_CODE
+    echo "Enter Your Name!"
+    read custName
 
-result=$(grep "$HSN_CODE" "$file_name")
+    while true; do
 
-if [ -n "$result" ]; then
-    Prod_Desc=$(echo "$result" | awk -F' ' '{print $2}')
-    echo "Prod_Desc: $Prod_Desc"
-else
-    echo "Entry with HSN_CODE $HSN_CODE not found in $file_name"
-    break
-fi
+        echo "Enter HSN code"
+        read HSN_CODE
 
-echo "Enter Quantity"
-read Quantity
+        result=$(grep "$HSN_CODE" "$file_name")
 
-echo "Do you want to change Quantity ? "
-read changeQuantity
-
-if [ "$changeQuantity" -eq 1 ];
-then
-   echo "Enter Quantity"
-   read Quantity
-fi
-
-echo "Exit ??"
-read exit
-
-if [ "$exit" -eq 1 ];
-then
-   exit 0
-fi
-
-quantity=$(echo "$result" | awk -F' ' '{print $3}')
-if [ "$Quantity" -gt "$quantity"  ];
-then 
-	echo "Item not available"
-	break
-else
-	addItem $HSN_CODE $Quantity
-	echo "Do you want to add Item ? "
-	read add_Item
-fi
-
-if [ "$add_Item" -eq 0 ]; then
+        if [ -n "$result" ]; then
+            Prod_Desc=$(echo "$result" | awk -F' ' '{print $2}')
+            echo "Prod_Desc: $Prod_Desc"
+        else
+            echo "Entry with HSN_CODE $HSN_CODE not found in $file_name"
             break
-fi
+        fi
 
-done
-echo "Generate Bill!"
-read bill
+        echo "Enter Quantity"
+        read Quantity
 
-if [ "$bill" -ne 0 ];then
-    salesReport
-    updateWarehouse
-    billReport "$custName" "$totalBill"
-else
-exit 0
-fi
+        echo "Do you want to change Quantity ? "
+        read changeQuantity
+
+        if [ "$changeQuantity" -eq 1 ]; then
+            echo "Enter Quantity"
+            read Quantity
+        fi
+
+        echo "Exit ??"
+        read exit
+
+        if [ "$exit" -eq 1 ]; then
+            exit 0
+        fi
+
+        quantity=$(echo "$result" | awk -F' ' '{print $3}')
+        if [ "$Quantity" -gt "$quantity" ]; then
+            echo "Item not available"
+            break
+        else
+            addItem $HSN_CODE $Quantity
+            echo "Do you want to add Item ? "
+            read add_Item
+        fi
+
+        if [ "$add_Item" -eq 0 ]; then
+            break
+        fi
+
+    done
+    echo "Generate Bill!"
+    read bill
+
+    if [ "$bill" -ne 0 ]; then
+        salesReport
+        updateWarehouse
+        billReport "$custName" "$totalBill"
+    else
+        exit 0
+    fi
 done
